26cba5f342edd1bb38e08d3d430b337fd0fb44e0
Mrgoodfornothing <antidemagouge09@proton.me>
2025-12-11 14:50:41 +0530

    connect to live backend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,18 @@
-// src/main.jsx
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom' // <--- Import this
+import { BrowserRouter } from 'react-router-dom'
+import axios from 'axios' // <--- 1. Import Axios
 import App from './App.jsx'
 import './index.css'
 import { ThemeProvider } from './context/ThemeContext.jsx'
 
+// <--- 2. Add this line. PASTE YOUR RENDER URL INSIDE THE QUOTES --->
+axios.defaults.baseURL = 'https://society-app-backend.onrender.com'; 
+// (Example: 'https://society-app-backend-a1b2.onrender.com')
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter> {/* <--- Wrap everything in this */}
+    <BrowserRouter>
       <ThemeProvider>
         <App />
       </ThemeProvider>
